69bd080c429e065928892b526ea40988b8ba192b
rohitnv <san453@gmail.com>
2016-10-15 19:05:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,6 +106,16 @@ app.get('/article2', function (req, res) {
 app.get('/article3', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
 });
+//-----------Java script page response-----------------------------------------
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.jsl'));
+});
+
+
+
+
+
 
 
 app.get('/article4', function (req, res) {
